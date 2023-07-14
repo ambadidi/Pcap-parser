c41b51ba195b6ca85600f4d06830713cf2b4492b
ambadidi <amine.badidi@hotmail.com>
2023-07-14 12:47:17 +0100

    open file

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,3 +1,16 @@
+use std::fs::File;
+// use std::io::prelude::*;
+//mdf-kospi200.20110216-0.pcap
 fn main() {
-    println!("Hello, world!");
+    // Open a file in read only mode in the local file system
+    let data_result = File::open("../../mdf-kospi200.20110216-0.pcap");
+
+    // Reading a file returns a Result enum
+    // Result can be a file or an error
+    let data_file = match data_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the data file: {:?}", error),
+    };
+
+    println!("Data file: {:?}", data_file);
 }
