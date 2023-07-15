8deedda8bcaf8dd15b0a6d192b495daa370fc6d9
ambadidi <amine.badidi@hotmail.com>
2023-07-15 09:09:04 +0100

    trying pcap-file

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,19 +1,31 @@
-use std::fs::File;
+use pcap_file::pcap::PcapReader;
 use std::env;
+use std::fs::File;
 // use std::io::prelude::*;
 //mdf-kospi200.20110216-0.pcap
 fn main() {
     let args: Vec<String> = env::args().collect();
     let pcap_file = &args[1];
-    // Open a file in read only mode in the local file system
+
     // let data_result = File::open("../../mdf-kospi200.20110216-0.pcap");
     let data_result = File::open(pcap_file);
-    // Reading a file returns a Result enum
-    // Result can be a file or an error
+
     let data_file = match data_result {
         Ok(file) => file,
         Err(error) => panic!("Problem opening the data file: {:?}", error),
     };
 
-    println!("Data file: {:?}", data_file);
+    // println!("Data file: {:?}", data_file);
+    let mut pcap_reader = PcapReader::new(data_file).unwrap();
+    // Read test.pcap
+    let mut i = 0;
+    while let Some(pkt) = pcap_reader.next_packet() {
+        //Check if there is no error
+        let pkt = pkt.unwrap().into_owned();
+
+        //Do something
+        println!("{:?}", pkt.timestamp);
+        i += 1;
+        if i == 5 {break;}
+    }
 }
